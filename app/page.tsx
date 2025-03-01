d31a5dd314ac2c41cbb5ef5f9aae8c75e8f58f5e
vijaykanth dondapati <vijaykanthdondapati@vijaykanths-MacBook-Pro.local>
2025-03-01 13:34:18 +0530

    search component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 //import Image from "next/image";
 import NumberList from "./listing";
+import Search from "./search";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
     <div className="bg-white">
+      <Search />
       <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
         <h2 className="text-2xl font-bold tracking-tight text-gray-900">
           Listing
